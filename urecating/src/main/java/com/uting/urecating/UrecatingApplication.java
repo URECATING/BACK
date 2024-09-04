e9bdc4b1101bbe9a240db6e0ebb74827a053c746
YaeJinKwon <93317276+YaeJinKwon@users.noreply.github.com>
2024-09-04 09:33:20 +0900

    Spring start
    Lets start!

---
+++ urecating/src/main/java/com/uting/urecating/UrecatingApplication.java
@@ -0,0 +1,13 @@
+package com.uting.urecating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecatingApplication.class, args);
+	}
+
+}
---
+++ urecating/src/test/java/com/uting/urecating/UrecatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uting.urecating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
