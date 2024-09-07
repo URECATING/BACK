599e7d48ebee23a789a176654b59c1949b256e1b
rladbstn1000 <614edison@gmail.com>
2024-09-07 16:37:40 +0900

    feat: 게시글 repository

---
+++ urecating/src/main/java/com/uting/urecating/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.uting.urecating.repository;
+
+import com.uting.urecating.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUserId(Long userId);
+}
