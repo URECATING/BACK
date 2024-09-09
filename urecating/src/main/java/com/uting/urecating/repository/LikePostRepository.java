9d60da724f258aad83885a9a87ebc3974aafa731
YaeJinKwon <93317276+YaeJinKwon@users.noreply.github.com>
2024-09-09 11:01:10 +0900

    feat : Add Comment, LikePost , Join entity & repo

--- urecating/src/main/java/com/uting/urecating/repository/LikePostRepository.java
+++ urecating/src/main/java/com/uting/urecating/repository/LikePostRepository.java
@@ -1,6 +1,8 @@
 package com.uting.urecating.repository;
 
 import com.uting.urecating.domain.LikePost;
+import com.uting.urecating.domain.Post;
+import com.uting.urecating.domain.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,12 +10,12 @@ import java.util.List;
 public interface LikePostRepository extends JpaRepository<LikePost, Long> {
 
     // 유저가 게시물에 좋아요를 했는지 확인
-    boolean existsByUserPostId(Long userId, Long postId);
+    boolean existsByUserAndPost(SiteUser user, Post post);
 
     // 게시물의 좋아요 개수 확인
-    int countByPostId(Long postId);
+    int countByPost(Post post);
 
     // 좋아요 목록을 조회
-    List<LikePost> findByUserId(Long userId);
+    List<LikePost> findByUser(SiteUser user);
 
 }
