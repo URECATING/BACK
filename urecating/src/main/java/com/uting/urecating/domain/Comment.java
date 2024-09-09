b909f7a71648f1362840548de4a01203fd1534bc
YaeJinKwon <93317276+YaeJinKwon@users.noreply.github.com>
2024-09-09 10:36:36 +0900

    feat : Add Comment, LikePost , Join entity & repo

---
+++ urecating/src/main/java/com/uting/urecating/domain/Comment.java
@@ -0,0 +1,39 @@
+package com.uting.urecating.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "comment_id")
+    private long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "parent_id")
+    private Comment parent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private SiteUser user;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true, cascade = CascadeType.ALL ) // 부모댓글이 삭제되면 자식 댓글도 삭제.
+    private List<Comment> children = new ArrayList<>();
+
+
+}
---
+++ urecating/src/main/java/com/uting/urecating/repository/JoinRepository.java
@@ -0,0 +1,13 @@
+package com.uting.urecating.repository;
+
+
+import com.uting.urecating.domain.Join;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JoinRepository extends JpaRepository<Join, Long> {
+
+    // 참여 목록 조회
+    List<Join> findByUserId(Long userId);
+}
