9aa1f6ecbb828924e2cbd5536f2dbd580b48e67c
YaeJinKwon <93317276+YaeJinKwon@users.noreply.github.com>
2024-09-09 11:01:10 +0900

    feat : Add Comment, LikePost , Join entity & repo

--- urecating/src/main/java/com/uting/urecating/repository/JoinRepository.java
+++ urecating/src/main/java/com/uting/urecating/repository/JoinRepository.java
@@ -2,6 +2,8 @@ package com.uting.urecating.repository;
 
 
 import com.uting.urecating.domain.Join;
+import com.uting.urecating.domain.LikePost;
+import com.uting.urecating.domain.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,5 @@ import java.util.List;
 public interface JoinRepository extends JpaRepository<Join, Long> {
 
     // 참여 목록 조회
-    List<Join> findByUserId(Long userId);
+    List<Join> findByUser(SiteUser user);
 }
