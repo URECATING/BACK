37093f7edf18445dbd7753df44bfba771361431f
taeyeon0319 <xodus1349@likelion.org>
2024-09-09 13:36:36 +0900

    feat : Password암호화

--- urecating/src/main/java/com/uting/urecating/service/UserService.java
+++ urecating/src/main/java/com/uting/urecating/service/UserService.java
@@ -4,20 +4,28 @@ import com.uting.urecating.domain.SiteUser;
 import com.uting.urecating.dto.UserUpdateDto;
 import com.uting.urecating.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
+
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     // 회원가입
     public SiteUser join(String userName, String login, String password,
                          String team, String gender, String phone) {
+        if(userRepository.existsByLogin(login)){
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
         SiteUser user = SiteUser.builder()
                 .userName(userName)
                 .login(login)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .team(team)
                 .gender(gender)
                 .phone(phone)
@@ -31,7 +39,7 @@ public class UserService {
         SiteUser user = userRepository.findByLogin(login)
                 .orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
 
-        if(!user.getPassword().equals(password)){
+        if(!passwordEncoder.matches(password, user.getPassword())){
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
@@ -47,6 +55,10 @@ public class UserService {
     //마이페이지 수정 - 이미지, 비밀번호, 전화번호
     public SiteUser updateUser(Long id, UserUpdateDto updateDto) {
         SiteUser user = getUserById(id);
+
+        String updatedPassword = updateDto.getPassword() !=null ?
+                passwordEncoder.encode(updateDto.getPassword()) : user.getPassword();
+
         SiteUser updatedUser = SiteUser.builder()
                 .id(user.getId())
                 .login(user.getLogin())
@@ -54,7 +66,7 @@ public class UserService {
                 .team(user.getTeam())
                 .gender(user.getGender())
                 .phone(updateDto.getPhone() != null ? updateDto.getPhone() : user.getPhone())
-                .password(updateDto.getPassword() != null ? updateDto.getPassword() : user.getPassword())
+                .password(updatedPassword)
                 .image(updateDto.getImage() != null ? updateDto.getImage() : user.getImage())
                 .build();
         return userRepository.save(updatedUser);
