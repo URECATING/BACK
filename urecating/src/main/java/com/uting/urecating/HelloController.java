076158009dd010741f9b073166508dd82b5e1bba
taeyeon0319 <xodus1349@likelion.org>
2024-09-13 23:21:11 +0900

    fix: 최신코드반영 확인

--- urecating/src/main/java/com/uting/urecating/HelloController.java
+++ urecating/src/main/java/com/uting/urecating/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping("/")
     @ResponseBody
     public String hi() {
-        return "UrecaTing - I'm home";
+        return "UrecaTing!!!";
     }
 
     @GetMapping("/springfoxtest")
