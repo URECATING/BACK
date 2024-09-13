d09753fe918bff97b4a5bee6b3980b09b3cdc26b
taeyeon0319 <xodus1349@likelion.org>
2024-09-13 20:15:40 +0900

    fix: taeyeon

--- urecating/src/main/java/com/uting/urecating/HelloController.java
+++ urecating/src/main/java/com/uting/urecating/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping("/")
     @ResponseBody
     public String hi() {
-        return "UrecaTing - SOS...!!!!";
+        return "UrecaTing - SOS..Taeyeon!!!";
     }
 
     @GetMapping("/springfoxtest")
