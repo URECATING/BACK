7c0371795e88d6156c77221e6cac6ce3d7961dd0
rladbstn1000 <614edison@gmail.com>
2024-09-20 15:40:19 +0900

    feat: PostDetailDto team 추가

--- urecating/src/main/java/com/uting/urecating/dto/PostDetailDto.java
+++ urecating/src/main/java/com/uting/urecating/dto/PostDetailDto.java
@@ -5,13 +5,14 @@ import com.uting.urecating.domain.Post;
 
 import java.time.LocalDateTime;
 
-public record PostDetailDto(Long userId, String username, String userImage, String title, String content, String image, Category category, LocalDateTime meetingDate, String place, int maxCapacity, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record PostDetailDto(Long userId, String username, String userImage, String team, String title, String content, String image, Category category, LocalDateTime meetingDate, String place, int maxCapacity, LocalDateTime createdAt, LocalDateTime updatedAt) {
 
     public static PostDetailDto fromPost(Post p) {
         return new PostDetailDto(
                 p.getUser().getId(),
                 p.getUser().getUserName(),
                 p.getUser().getImage(),
+                p.getUser().getTeam(),
                 p.getTitle(),
                 p.getContent(),
                 p.getImage(),
