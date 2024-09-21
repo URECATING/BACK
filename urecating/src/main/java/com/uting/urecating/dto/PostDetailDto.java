68da2031b49b0433d1d6a85748bc15590997a31b
rladbstn1000 <614edison@gmail.com>
2024-09-21 17:03:34 +0900

    feat: PostDetailDto status String으로 변경

--- urecating/src/main/java/com/uting/urecating/dto/PostDetailDto.java
+++ urecating/src/main/java/com/uting/urecating/dto/PostDetailDto.java
@@ -5,9 +5,16 @@ import com.uting.urecating.domain.Post;
 
 import java.time.LocalDateTime;
 
-public record PostDetailDto(Long userId, String username, String userImage, String team, String title, String content, String image, Category category, LocalDateTime meetingDate, String place, int maxCapacity, LocalDateTime createdAt, LocalDateTime updatedAt, boolean status) {
+public record PostDetailDto(Long userId, String username, String userImage, String team, String title, String content, String image, Category category, LocalDateTime meetingDate, String place, int maxCapacity, LocalDateTime createdAt, LocalDateTime updatedAt, String status) {
 
     public static PostDetailDto fromPost(Post p) {
+        String status;
+        if(p.isStatus()) {
+            status = "모집중";
+        }else{
+            status = "모집완료";
+        }
+
         return new PostDetailDto(
                 p.getUser().getId(),
                 p.getUser().getUserName(),
@@ -22,6 +29,6 @@ public record PostDetailDto(Long userId, String username, String userImage, Stri
                 p.getMaxCapacity(),
                 p.getCreatedAt(),
                 p.getUpdatedAt(),
-                p.isStatus());
+                status);
     }
 }
